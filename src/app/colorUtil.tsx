1004f7f2cfae310a515225dcf274bd7ecc2fcc22
chris.bartolo <chris@chrisbartolo.com>
2023-05-04 10:24:39 +0200

    remove redundant files which were moved. Change deprecated functions

--- src/app/colorUtil.tsx
+++ src/app/colorUtil.tsx
@@ -14,10 +14,9 @@ export const getRGBColorRaw = (hex: string | undefined) => {
     if (hex) {
         let color = hex.replace(/#/g, "")
         // rgb values
-        let r = parseInt(color.substr(0, 2), 16)
-        let g = parseInt(color.substr(2, 2), 16)
-        let b = parseInt(color.substr(4, 2), 16)
-
+        const r = parseInt(color.substring(0, 2), 16)
+        const g = parseInt(color.substring(2, 4), 16)
+        const b = parseInt(color.substring(4, 6), 16)
         return `${r}, ${g}, ${b}`
     } else {
         return `0,0,0`;
@@ -30,9 +29,9 @@ export const getRGBColorRaw = (hex: string | undefined) => {
 export const getAccessibleColor = (hex: string) => {
     let color = hex.replace(/#/g, "")
     // rgb values
-    var r = parseInt(color.substr(0, 2), 16)
-    var g = parseInt(color.substr(2, 2), 16)
-    var b = parseInt(color.substr(4, 2), 16)
-    var yiq = (r * 299 + g * 587 + b * 114) / 1000
+    const r = parseInt(color.substring(0, 2), 16)
+    const g = parseInt(color.substring(2, 4), 16)
+    const b = parseInt(color.substring(4, 6), 16)
+    const yiq = (r * 299 + g * 587 + b * 114) / 1000
     return yiq >= 128 ? "#000000" : "#FFFFFF"
 }
